4160b5ea65169f982fcd71cb9e2b2707c8f899c2
UkiDelly <daehyeon1999@naver.com>
2023-10-25 23:24:32 +0900

    [ADD,UPDATE] AWS S3 연결 설정
    - AWS S3 연결에 필요한 설정 완료

--- myblog/settings.py
+++ myblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from typing import Final
 
 import environ
 
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "accounts",
     "blog",
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -127,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+# STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -136,5 +138,22 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.User"
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = BASE_DIR / "media"
+
+# AWS Setting
+AWS_REGION: Final[str] = "ap-northeast-2"
+AWS_STORAGE_BUCKET_NAME: Final[str] = "delly-djangoblog"
+AWS_ACCESS_KEY_ID: Final[str] = env("AWS_ACCESS")
+AWS_SECRET_ACCESS_KEY: Final[str] = env("AWS_SECRET_ACCESS")
+
+AWS_S3_CUSTOM_DOMAIN: Final = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com"
+
+# Static Settings
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+# Media Setting
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
