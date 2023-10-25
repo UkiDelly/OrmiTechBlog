af7de68a248783a48b972d02cbe9be9f2c720d1a
UkiDelly <daehyeon1999@naver.com>
2023-10-25 16:49:14 +0900

    [ADD] accounts , blog 앱 추가

---
+++ accounts/models.py
@@ -0,0 +1,66 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+
+# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, name, username, email, password):
+        if not name:
+            raise ValueError("must have user email")
+        if not email:
+            raise ValueError("must have user email")
+        user = self.model(
+            name=name,
+            username=username,
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, name, username, email, password):
+        user = self.create_user(
+            name=name,
+            email=self.normalize_email(email),
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+    )
+    username = models.CharField(max_length=100, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = [
+        "email",
+        "name",
+        "username",
+    ]
+
+    def __str__(self):
+        return self.name
+
+
+def has_perm(self, perm, obj=None):
+    return True
+
+
+def has_module_perms(self, app_label):
+    return True
+
+
+@property
+def is_staff(self):
+    return self.is_admin
