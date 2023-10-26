cb13330561467ea82fea4d0fabd364c53c7a4a23
UkiDelly <daehyeon1999@naver.com>
2023-10-26 17:00:19 +0900

    [ADD] accounts url에 별칭 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from accounts.views import UserRegisterView, UserLoginView
 
+app_name = "accounts"
 urlpatterns = [
     path("signup/", UserRegisterView.as_view(), name="signup"),
     path("login/", UserLoginView.as_view(), name="login"),
