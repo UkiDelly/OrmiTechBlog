1653f00e679fd0b4aed1f8f49cff212808dbc7d4
UkiDelly <daehyeon1999@naver.com>
2023-10-26 11:44:18 +0900

    [ADD] Blog 모델 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
+from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
 
+from accounts.models import User
+
+
 # Create your models here.
+class BaseDateTime(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Blog(BaseDateTime):
+    title = models.CharField(max_length=100)
+    content = RichTextUploadingField(blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=False)
+    tags = models.CharField(max_length=100, blank=True)
+    view_count = models.IntegerField(default=0)
+    likes = models.ManyToManyField(User, blank=True, related_name="blog_like")
+
+    class Meta:
+        db_table = "blog"
+
+
+class Comment(BaseDateTime):
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=False)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    parent_comment = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True
+    )
+
+    class Meta:
+        db_table = "comment"
