c6e6a1e926bfc018ad26d862a1597f94783c7fe5
UkiDelly <daehyeon1999@naver.com>
2023-10-26 10:49:06 +0900

    [UPDATE] AWS S3 연결 끊음(개발 할때만)

--- myblog/settings.py
+++ myblog/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = "/static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -138,8 +138,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.User"
 
-# MEDIA_URL = "/media/"
-# MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # AWS Setting
 AWS_REGION: Final[str] = "ap-northeast-2"
@@ -149,11 +149,11 @@ AWS_SECRET_ACCESS_KEY: Final[str] = env("AWS_SECRET_ACCESS")
 
 AWS_S3_CUSTOM_DOMAIN: Final = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com"
 
-# Static Settings
-STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# Static Settings ()
+# STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-# Media Setting
-MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+######## Media Setting (배포할때만)
+# MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
