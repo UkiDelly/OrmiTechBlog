fe2c8832dee05ee98e77f73c06d4d867d6506ce0
UkiDelly <daehyeon1999@naver.com>
2023-10-26 22:12:28 +0900

    [ADD,UPDATE] view 개발, url등록
    - Blog 리스트의 정렬을 최신순으로 설정
    - 검색 기능 개발중
    - FBV -> BlogDetailView 로 변경
    - BlogCreationForm에 CategoryForm도 추가
    - BlogUpdateForm 개발
    - Comment 생성 개발
    - CommentUpdate 개발중
    - 카테고리 생성 개발중

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 
-from blog.views import BlogListView, BlogCreateView, get_detail_view
+from blog.views import (
+    BlogCreateView,
+    BlogListView,
+    create_comment,
+    BlogDetailView,
+    BlogUpdateView,
+    add_new_category,
+)
 
+app_name = "blog"
 urlpatterns = [
     path("", BlogListView.as_view(), name="blog_list"),
-    path("<int:pk>/", get_detail_view, name="blog_detail"),
+    path("<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
     path("create/", BlogCreateView.as_view(), name="blog_create"),
+    path("<int:pk>/comment/new", create_comment, name="comment_new"),
+    path("<int:pk>/update/", BlogUpdateView.as_view(), name="blog_update"),
+    path("tag/new", add_new_category, name="tag_new"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,50 +1,130 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpRequest
-from django.shortcuts import render
+from django.db.models import Q
+from django.http import HttpRequest, JsonResponse, HttpResponseBadRequest
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 
-from blog.models import Blog, Comment
-
-
-# Create your views here.
-# def blog_list(request: HttpRequest):
-#     blogs = Blog.objects.order_by("-created_at").all()
-#     blogs = [blog.toJson() for blog in blogs]
-#
-#     return JsonResponse({"result": blogs}, safe=False)
+from blog.forms import CommentForm, BlogForm, CategoryForm
+from blog.models import Blog, Comment, Category
 
 
 class BlogListView(ListView):
     model = Blog
     template_name = "blog/blog_list.html"
     context_object_name = "blogs"
+    ordering = "-created_at"
+
+    def get_queryset(self):
+        query_set = super().get_queryset()
+
+        # reqeust의 GET 파라미터에서 'q'를 가져옵니다.
+        q = self.request.GET.get("q")
+
+        # 'q' 파라미터가 제공되었을 경우, 쿼리셋을 필터링한다.
+        if q:
+            query_set = query_set.filter(
+                Q(title__icontains=q)
+                | Q(content__icontains=q)
+                | Q(tags__name__icontain=q)
+            )
+        return query_set
 
 
-# class BlogDetailView(DetailView):
-#     model = Blog
-#     template_name = "blog/blog_detail.html"
-#     context_object_name = "blog"
-#
-#     def get_object(self, queryset=None):
-#         blog = super().get_object(queryset)
-#         return blog
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = "blog/blog_detail.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs["pk"]
 
-def get_detail_view(reqeust: HttpRequest, pk: int):
-    blog = Blog.objects.get(pk=pk)
-    comment_query_set = Comment.objects.filter(blog_id__exact=pk).values()
-    context = {"blog": blog, "comments": comment_query_set}
-    return render(reqeust, "blog/blog_detail.html", context)
+        form = CommentForm(
+            initial={"author": self.request.user, "parent_comment": None, "blog": pk}
+        )
+        blog: Blog | None = get_object_or_404(Blog, pk=pk)
+        comments = blog.comment_set.all()
+        context["request"] = self.request
+        context["blog"] = blog
+        context["comments"] = comments
+        context["comment_form"] = form
+        return context
 
 
 class BlogCreateView(LoginRequiredMixin, CreateView):
-    model = Blog
+    form_class = BlogForm
     template_name = "blog/blog_form.html"
-    fields = ["title", "content", "tags"]
     success_url = reverse_lazy("blog_list")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["category_from"] = CategoryForm()
+        return context
 
     # catch the form when press submit
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class BlogUpdateView(LoginRequiredMixin, UpdateView):
+    form_class = BlogForm
+    model = Blog
+    template_name = "blog/blog_form.html"
+
+    def form_valid(self, form):
+        if form.is_valid():
+            form.save()
+            return redirect(reverse_lazy("blog:blog_detail", kwargs=self.kwargs))
+        return HttpResponseBadRequest
+
+
+@login_required(login_url=reverse_lazy("accounts:login"))
+def create_comment(request: HttpRequest, pk: int):
+    comment_form = CommentForm(request.POST)
+    parent_id = request.POST.get(key="parent_comment", default=None)
+    parent_comment = None
+    if parent_id is not None:
+        parent_comment = Comment.objects.get(pk=parent_id)
+    if comment_form.is_valid():
+        print(comment_form.cleaned_data)
+
+        Comment.objects.create(
+            content=comment_form.cleaned_data["content"],
+            author=request.user,
+            blog=comment_form.cleaned_data["blog"],
+            parent_comment=parent_comment,
+        )
+        return redirect("blog_detail", pk=pk)
+    return JsonResponse({"result": comment_form.errors}, status=400)
+
+
+class CommentUpdateView(LoginRequiredMixin, UpdateView):
+    form_class = CommentForm
+    model = Comment
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, instance=self.get_object())
+        if form.is_valid():
+            return super().post(request, *args, **kwargs)
+
+
+def add_new_category(request: HttpRequest):
+    category_form = CategoryForm(request.POST)
+    if category_form.is_valid():
+        category_form.save()
+
+        category = Category.objects.all()
+        return JsonResponse({"result": category}, status=200, safe=False)
+
+    return JsonResponse({"result": category_form.errors}, status=400)
+
+
+# TODO: JsonResponse를 사용해도 괜찮은지 물어보자
+
+# class CommentView(View):
+#
+#     def get(self,request:HttpRequest, pk:int):
+#         pass
+#     ...
