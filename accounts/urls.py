0814bcfa792e0617ba72f76b7cca7ba5fb072bf1
UkiDelly <daehyeon1999@naver.com>
2023-10-26 11:42:44 +0900

    [ADD] accounts api 개발
    - login , logout api도 개발 완료
    - 로그인 폼 개발
    - 로그인뷰, 로그아웃 뷰 개발

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-from accounts.views import UserRegisterView
+from accounts.views import UserRegisterView, UserLoginView
 
-urlpatterns = [path("signup/", UserRegisterView.as_view(), name="signup")]
+urlpatterns = [
+    path("signup/", UserRegisterView.as_view(), name="signup"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", UserLoginView.as_view(), name="logout"),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 # Create your views here.
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView
 
 from accounts.forms import UserCreationForm
@@ -8,3 +9,12 @@ class UserRegisterView(CreateView):
     form_class = UserCreationForm
     success_url = "/"
     template_name = "accounts/user_create_form.html"
+
+
+class UserLoginView(LoginView):
+    template_name = "accounts/user_login_form.html"
+    next_page = "/"
+
+
+class UserLogoutView(LogoutView):
+    next_page = "/"
