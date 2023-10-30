9c9e15695fd379cc4be845c5d1879d65d7a319b0
UkiDelly <daehyeon1999@naver.com>
2023-10-30 11:53:30 +0900

    [ADD] 내 정보 조회 api 개발
    - 내 정보를 조회 할수 있는 api 개발

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from accounts.views import UserRegisterView, UserLoginView, get_token, UserLogoutView
+from accounts.views import (
+    UserRegisterView,
+    UserLoginView,
+    get_token,
+    UserLogoutView,
+    UserInfoView,
+)
 
 app_name = "accounts"
 urlpatterns = [
+    path("myinfo/<int:pk>/", UserInfoView.as_view(), name="my_info"),
     path("signup/", UserRegisterView.as_view(), name="signup"),
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", UserLogoutView.as_view(), name="logout"),
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,13 @@
 # Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpRequest, JsonResponse
 from django.middleware import csrf
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accounts.forms import UserCreationForm
+from accounts.models import User
 
 
 class UserRegisterView(CreateView):
@@ -21,6 +23,16 @@ class UserLoginView(LoginView):
 
 class UserLogoutView(LogoutView):
     next_page = reverse_lazy("blog:blog_list")
+
+
+class UserInfoView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = "accounts/my_info.html"
+
+    def get_context_data(self, **kwargs):
+        user = self.get_object()
+        context = {"user": user.toJson()}
+        return context
 
 
 def get_token(request: HttpRequest):
