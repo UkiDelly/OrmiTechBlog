a4b93460b316c5ebba24b8f6ee87f7bf53370949
UkiDelly <daehyeon1999@naver.com>
2023-10-31 15:18:26 +0900

    [ADD,UPDATE] 대댓글 API 완성, token API 수정
    - 대댓글 API 완성
    - token API에 로그인 필수 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from accounts.views import UserRegisterView, UserLoginView, get_token, UserLogoutView
@@ -7,5 +8,5 @@ urlpatterns = [
     path("signup/", UserRegisterView.as_view(), name="signup"),
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", UserLogoutView.as_view(), name="logout"),
-    path("token/", get_token, name="get_token"),
+    path("token/", login_required(get_token), name="get_token"),
 ]
